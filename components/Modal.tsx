2f4ed8aa646c9d1cf5e2bef4978f82912a2b55e5
rizwan <rizwananwar230698@gmail.com>
2021-11-18 00:50:35 +0530

    UI: modal open animation

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -6,8 +6,8 @@ export type ModalProps = {
 
 const Modal = ({ children }: ModalProps) => {
   return (
-    <div className="flex justify-center items-center fixed inset-0 h-screen w-screen bg-black bg-opacity-70 p-2 lg:p-0">
-      <div className="flex flex-col rounded-md p-4 bg-white w-full max-w-md">
+    <div className="modal-overlay flex justify-center items-center fixed inset-0 h-screen w-screen bg-black bg-opacity-70 p-2 lg:p-0">
+      <div className="modal flex flex-col rounded-md p-4 bg-white w-full max-w-md">
         {children}
       </div>
     </div>
